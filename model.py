ffbfd6248fdba27d42abb9f525b02e5ddfe27b1f
Nitish <nitishpandey2117@gmail.com>
2025-04-26 11:43:10 +0530

    create model.py

---
+++ model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Seq2SeqLM(nn.Module):
+    def __init__(self):
+        super(Seq2SeqLM, self).__init__()
+
+    def forward(self):
+        pass
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def forward(self):
+        pass
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+
+    def forward(self):
+        pass
+
+class DecoderLayer(nn.Module):
+    def __init__(self):
+        super(DecoderLayer, self).__init__()
+
+    def forward(self):
+        pass
+
